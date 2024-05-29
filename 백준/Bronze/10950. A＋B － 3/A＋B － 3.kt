d3a8b9ee31652770b6d3998d453db5104efab692
JasonHwang <junghahwang0601@gmail.com>
2024-05-30 00:40:12 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 13524 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(){
+    val s = Scanner(System.`in`)
+
+    var a = s.nextInt()
+    for(a in 1..a){
+        var b = s.nextInt()
+        var c = s.nextInt()
+        println(b+c)
+    }
+}
