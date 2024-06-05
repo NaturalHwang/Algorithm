9b43988fe54785760633c042418621966c95a984
JasonHwang <junghahwang0601@gmail.com>
2024-06-05 15:12:50 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 140 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int []chess = {1,1,2,2,2,8};
+
+        int []own = new int[6];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < 6; i++){
+            own[i] = Integer.parseInt(st.nextToken());
+            System.out.print(chess[i] - own[i] + " ");
+        }
+    }
+}
