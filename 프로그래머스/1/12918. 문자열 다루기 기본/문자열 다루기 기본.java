ccae68ff46c10ce560faff150a818c2d6aebb6e7
JasonHwang <junghahwang0601@gmail.com>
2024-06-07 12:14:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.10 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() == 4 || s.length() == 6){
+            answer = s.matches("[0-9]+");
+        } else answer = false;
+        
+        return answer;
+    }
+}
