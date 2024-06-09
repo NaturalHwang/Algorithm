422fd06b23b74858a05d3a3c667b5c9ea793caf5
JasonHwang <junghahwang0601@gmail.com>
2024-06-09 20:49:39 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int maxScore = arr[N - 1];
+
+        for (int i = 0; i < N; i++) {
+            double newScore = (double) arr[i] / maxScore * 100;
+            sum += newScore;
+        }
+
+        double avg = sum / N;
+        System.out.println(avg);
+    }
+}
