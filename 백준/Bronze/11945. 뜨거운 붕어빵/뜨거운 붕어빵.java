1146f631bcf515b87adc6f08e3dce39c4f2e5ef1
JasonHwang <junghahwang0601@gmail.com>
2024-06-15 20:53:04 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 180 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        if(M > 0){
+            for(int i = 0; i < N; i++){
+                char[] boong = sc.next().toCharArray();
+                for(int j = M -1; j >= 0; j--){
+                    System.out.print(boong[j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
