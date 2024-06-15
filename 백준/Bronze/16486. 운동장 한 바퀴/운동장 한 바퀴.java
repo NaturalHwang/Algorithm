99d03663c239b2a0b211fec41570a89cd3bd6f75
JasonHwang <junghahwang0601@gmail.com>
2024-06-16 01:10:07 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 216 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double d1 = sc.nextDouble();
+        double d2 = sc.nextDouble();
+        double pi = 3.141592;
+
+        System.out.println(2*d1 + 2*d2*pi);
+    }
+}
